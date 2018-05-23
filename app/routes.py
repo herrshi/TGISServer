40319f863b85ad1183b8a51fba2877837e493cf7
晓青 史 <herrshi@hotmail.com>
2018-05-23 16:22:28 +0800

    11

--- app/routes.py
+++ app/routes.py
@@ -8,6 +8,11 @@ def index():
     return render_template('index.html')
 
 
+@app.route('/TGISServer')
+def server_home():
+    return render_template('ServerHome.html')
+
+
 @app.route('/TGISServer/GeometryServer/length')
 def length():
     points_param = request.args.get('points')
