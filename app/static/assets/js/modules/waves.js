455a8aadb7f9ba6df10e80df6bd77fe6e99ef714
晓青 史 <herrshi@hotmail.com>
2018-06-07 17:29:27 +0800

    11

--- app/static/assets/js/modules/waves.js
+++ app/static/assets/js/modules/waves.js
@@ -589,6 +589,7 @@
 //Initialization
 Waves.attach('.btn:not(.btn-flat), .btn-floating', ['waves-light']);
 Waves.attach('.btn-flat', ['waves-effect']);
+Waves.attach('.chip', ['waves-effect']);
 Waves.attach('.view a .mask', ['waves-light']);
 Waves.attach('.waves-light', ['waves-light']);
 Waves.attach('.navbar-nav a:not(.navbar-brand), .nav-icons li a, .nav-tabs .nav-item:not(.dropdown)', ['waves-light']);
