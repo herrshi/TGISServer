094d3e4103ba55de9d73f961aa39703876761e59
晓青 史 <herrshi@hotmail.com>
2018-06-14 17:40:54 +0800

    11

--- app/static/assets/js/scripts.js
+++ app/static/assets/js/scripts.js
@@ -3,4 +3,3 @@ $(".button-collapse").sideNav();
 // SideNav Scrollbar Initialization
 var sideNavScrollbar = document.querySelector(".custom-scrollbar");
 Ps.initialize(sideNavScrollbar);
-$(".show-me").click();
