0b0b82743dadb9ea6bab82477433236e081c7c66
晓青 史 <herrshi@hotmail.com>
2018-06-21 17:57:08 +0800

    11

--- app/static/assets/js/server/geometry_service_lengths.js
+++ app/static/assets/js/server/geometry_service_lengths.js
@@ -1,23 +1,33 @@
 require([
+  "esri/config",
   "esri/Map",
   "esri/views/MapView",
   "esri/Basemap",
+  "esri/Graphic",
   "esri/layers/TileLayer",
-  "esri/config",
+  "esri/layers/GraphicsLayer",
   "esri/widgets/CoordinateConversion",
+  "esri/widgets/Sketch/SketchViewModel",
   "esri/widgets/Home",
+  "esri/geometry/support/webMercatorUtils",
   "dojo/domReady!"
 ], function(
+  esriConfig,
   Map,
   MapView,
   Basemap,
+  Graphic,
   TileLayer,
-  esriConfig,
+  GraphicsLayer,
   CoordinateConversion,
-  Home
+  SketchViewModel,
+  Home,
+  webMercatorUtils
 ) {
   //允许跨域
   esriConfig.request.proxyUrl = "http://localhost:8090/proxy/proxy.jsp";
+
+  let drawLayer = new GraphicsLayer();
 
   const basemap = new Basemap({
     baseLayers: [
@@ -27,18 +37,21 @@ require([
     ]
   });
   const map = new Map({
-    basemap: basemap
+    basemap: basemap,
+    layers: [drawLayer]
   });
 
   let view = new MapView({
-    container: "mapViewDiv", // Reference to the DOM node that will contain the view
-    map: map, // References the map object created in step 3
-    zoom: 16, // Sets zoom level based on level of detail (LOD)
-    center: [121.414, 31.242677] // Sets center point of view using longitude,latitude
+    container: "mapViewDiv",
+    map: map,
+    zoom: 16,
+    center: [121.414, 31.242677]
   });
 
+  //隐藏地图服务信息
   view.ui.remove("attribution");
 
+  //增加Home按钮
   const homeWidget = new Home({
     view: view
   });
@@ -49,4 +62,94 @@ require([
   // });
   //
   // view.ui.add(ccWidget, "bottom-left");
+
+  const pointSymbol = {
+    type: "simple-marker", // autocasts as new SimpleMarkerSymbol()
+    style: "circle",
+    color: "#8A2BE2",
+    size: "16px",
+    outline: {
+      // autocasts as new SimpleLineSymbol()
+      color: [255, 255, 255],
+      width: 2 // points
+    }
+  };
+  const polylineSymbol = {
+    type: "simple-line", // autocasts as new SimpleLineSymbol()
+    color: "#8A2BE2",
+    width: "2",
+    style: "solid"
+  };
+  const polygonSymbol = {
+    type: "simple-fill", // autocasts as new SimpleFillSymbol()
+    color: "rgba(138,43,226, 0.8)",
+    style: "solid",
+    outline: {
+      color: "white",
+      width: 1
+    }
+  };
+
+  let sketchViewModel;
+  view.when(function() {
+    // create a new sketch view model
+    sketchViewModel = new SketchViewModel({
+      view: view,
+      layer: drawLayer,
+      pointSymbol: pointSymbol,
+      polylineSymbol: polylineSymbol,
+      polygonSymbol: polygonSymbol
+    });
+
+    sketchViewModel.on("draw-complete", addGraphic);
+    // sketchViewModel.on("update-complete", addGraphic);
+    // sketchViewModel.on("update-cancel", addGraphic);
+
+    sketchViewModel.create("polyline");
+  });
+
+  function addGraphic(evt) {
+    const geometry = evt.geometry;
+    let symbol;
+
+    // Choose a valid symbol based on return geometry
+    switch (geometry.type) {
+      case "point":
+        symbol = pointSymbol;
+        break;
+      case "polyline":
+        symbol = polylineSymbol;
+        break;
+      default:
+        symbol = polygonSymbol;
+        break;
+    }
+    // Create a new graphic; add it to the GraphicsLayer
+    const graphic = new Graphic({
+      geometry: geometry,
+      symbol: symbol
+    });
+    drawLayer.add(graphic);
+
+    //将当前显示的graphic转换为polylines参数格式
+    //[<polyline1>, <polyline2>, ..., <polylineN>]
+    let polylines = drawLayer.graphics.map(graphic => {
+      let polyline = graphic.geometry;
+      if (view.spatialReference.isWebMercator) {
+        polyline = webMercatorUtils.webMercatorToGeographic(polyline);
+      }
+      let jsonObj = polyline.toJSON();
+      delete jsonObj.spatialReference;
+      //坐标保留6位小数，更美观
+      jsonObj.paths.forEach(path => {
+        path.forEach(point => {
+          point[0] = Number(point[0]).toFixed(6);
+          point[1] = Number(point[1]).toFixed(6);
+        });
+      });
+      return jsonObj;
+    });
+
+    $("#text-polylines").val(JSON.stringify(polylines));
+  }
 });
