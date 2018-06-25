2e04c6322508f989a7113c4c09713d55235bff5f
晓青 史 <herrshi@hotmail.com>
2018-06-25 18:19:02 +0800

    11

--- app/tgis_server/geometry_service.py
+++ app/tgis_server/geometry_service.py
@@ -1,8 +1,11 @@
 from shapely.geometry import LineString
 from shapely.ops import transform
 from functools import partial
+import pyproj
 
 
 def length(params):
     line = LineString([(121.411243, 31.244787), (121.414912, 31.243429)])
-    return line.length
+    geod = pyproj.Geod(ellps='WGS84')
+    angle1, angle2, distance = geod.inv(121.411243, 31.244787, 121.414912, 31.243429)
+    return distance
