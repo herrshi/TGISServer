8e0ba28ea4916c6fc9ec26bda6a87718d68a4d94
herrshi <herrshi1980@gmail.com>
2018-07-03 23:07:27 +0800

    111

--- app/static/assets/js/map/map.js
+++ app/static/assets/js/map/map.js
@@ -1,7 +1,47 @@
 /// <amd-dependency path="esri/core/tsSupport/declareExtendsHelper" name="__extends" />
 /// <amd-dependency path="esri/core/tsSupport/decorateHelper" name="__decorate" />
-define(["require", "exports", "esri/core/tsSupport/declareExtendsHelper", "esri/core/tsSupport/decorateHelper"], function (require, exports, __extends, __decorate) {
+define(["require", "exports", "esri/core/tsSupport/declareExtendsHelper", "esri/core/tsSupport/decorateHelper", "esri/Map", "esri/views/MapView", "esri/Basemap", "esri/layers/TileLayer", "esri/widgets/Home", "esri/views/2d/draw/Draw"], function (require, exports, __extends, __decorate, EsriMap, MapView, Basemap, TileLayer, HomeWidget, Draw) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
+    var Map = /** @class */ (function () {
+        function Map(divName) {
+            var _this = this;
+            var basemap = new Basemap({
+                baseLayers: [
+                    new TileLayer({
+                        url: "https://map.geoq.cn/arcgis/rest/services/ChinaOnlineCommunity/MapServer"
+                    })
+                ]
+            });
+            var map = new EsriMap({
+                basemap: basemap
+            });
+            this.view = new MapView({
+                container: divName,
+                map: map,
+                zoom: 16,
+                center: [121.414, 31.242677]
+            });
+            this.view.ui.remove("attribution");
+            var homeWidget = new HomeWidget({
+                view: this.view
+            });
+            this.view.ui.add(homeWidget, "top-left");
+            this.view.when(function () {
+                _this.draw = new Draw({
+                    view: _this.view
+                });
+            });
+        }
+        Map.prototype.startDraw = function (drawType) {
+            var action = this.draw.create(drawType, {
+                mode: "click"
+            });
+        };
+        Map.prototype._addGraphic = function () {
+        };
+        return Map;
+    }());
+    exports.Map = Map;
 });
 //# sourceMappingURL=map.js.map
--- app/static/assets/js/map/map.ts
+++ app/static/assets/js/map/map.ts
@@ -2,8 +2,57 @@
 /// <amd-dependency path="esri/core/tsSupport/decorateHelper" name="__decorate" />
 
 import EsriMap = require("esri/Map");
-import EsriConfig = require("esri/config");
 import MapView = require("esri/views/MapView");
 import Basemap = require("esri/Basemap");
 import TileLayer = require("esri/layers/TileLayer");
 import HomeWidget = require("esri/widgets/Home");
+import Draw = require("esri/views/2d/draw/Draw");
+
+export class Map {
+  readonly view: MapView;
+  private draw: Draw;
+
+  constructor(divName: string) {
+    const basemap = new Basemap({
+      baseLayers: [
+        new TileLayer({
+          url:
+            "https://map.geoq.cn/arcgis/rest/services/ChinaOnlineCommunity/MapServer"
+        })
+      ]
+    });
+
+    const map = new EsriMap({
+      basemap: basemap
+    });
+
+    this.view = new MapView({
+      container: divName,
+      map: map,
+      zoom: 16,
+      center: [121.414, 31.242677]
+    });
+    this.view.ui.remove("attribution");
+
+    const homeWidget = new HomeWidget({
+      view: this.view
+    });
+    this.view.ui.add(homeWidget, "top-left");
+
+    this.view.when(() => {
+      this.draw = new Draw({
+        view: this.view
+      });
+    });
+  }
+
+  public startDraw(drawType: string) {
+    const action = this.draw.create(drawType, {
+      mode: "click"
+    });
+  }
+
+  private _addGraphic() {
+
+  }
+}
