d4e8433c30d00224d5d8e9a716a7dcc4bbf04148
晓青 史 <herrshi@hotmail.com>
2018-07-31 17:41:46 +0800

    11

---
+++ app/static/assets/js/server/network_service_route.js
@@ -0,0 +1,8 @@
+define(["require", "exports", "../map/map"], function (require, exports, map_1) {
+    "use strict";
+    Object.defineProperty(exports, "__esModule", { value: true });
+    const map = new map_1.Map("mapViewDiv");
+    map.createMap().then(() => {
+    });
+});
+//# sourceMappingURL=network_service_route.js.map
---
+++ app/static/assets/js/server/network_service_route.ts
@@ -0,0 +1,6 @@
+import { Map } from "../map/map";
+
+const map = new Map("mapViewDiv");
+map.createMap().then(() => {
+
+});
